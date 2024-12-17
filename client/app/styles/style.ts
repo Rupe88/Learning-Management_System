c53ee71269fdc67073ef48bede238325756206ef
Rupee88 <chyrupesh828@gmail.com>
2024-12-17 21:28:48 +0545

    react 19 init

--- client/app/styles/style.ts
+++
@@ -1,6 +0,0 @@
-export const styles = {
-    title: "text-2xl font-semibold text-center mb-6 text-gray-800 dark:text-gray-200",
-    label: "text-sm font-medium text-gray-700 dark:text-gray-300 mb-2 block",
-    input: "w-full text-gray-900 dark:text-gray-200 bg-gray-100 dark:bg-gray-700 border rounded-lg py-2 px-4 outline-none transition-all focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-600",
-    button: "w-full py-3 px-6 rounded-full text-white bg-blue-500 dark:bg-blue-600 font-semibold cursor-pointer transition-colors",
-  };
